9cfd7d9d282e90337411c41a2ff4c664d6a2f8f7
Michael Kavulich, Jr <kavulich@ucar.edu>
2024-08-22 19:36:57 +0000

    Finally figured out my dumb array slice problems, plot the first level of every variable now!

--- ush/plotting/plot_mpas_uxarray.py
+++ ush/plotting/plot_mpas_uxarray.py
@@ -1,6 +1,9 @@
+import sys
+
 import uxarray as ux
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 filename='history.2023-09-15_09.00.00.nc'
 
@@ -8,13 +11,22 @@ grid = ux.open_grid(filename)
 print(grid)
 
 uxds = ux.open_dataset(filename,filename)
-print(uxds)
 
-print(f"{uxds.uxgrid.sizes=}")
-var = "vegfra"
-print(f"{uxds[var]=}")
+for var in uxds:
+    if "n_face" not in uxds[var].dims:
+        print(f"Variable {var} not face-centered, skipping")
+        continue
+    print(f"Plotting variable {var}")
+    sliced=uxds[var]
+    if "Time" in sliced.dims:
+        print("Plotting first time step")
+        sliced=sliced.isel(Time=0)
+    if "nVertLevels" in sliced.dims:
+        print("Plotting first vertical level")
+        sliced=sliced.isel(nVertLevels=0)
 
-print(f"{uxds[var][:][0]=}")
+    print(sliced)
+
 #uxds["vegfra"][0].plot.rasterize(
 #    title="Grid plot through uxgrid attribute",
 #    backend="matplotlib",
@@ -22,17 +34,27 @@ print(f"{uxds[var][:][0]=}")
 #    fig_size=500,
 #)
 
-pc = uxds[var][:][0].to_polycollection()
+    try:
+        pc = sliced.to_polycollection()
+    except:
+        print(f"Could not convert {var} to polycollection; there may be additional dimensions here")
+        continue
 
-pc.set_antialiased(False)
+    pc.set_antialiased(False)
 
-pc.set_cmap("plasma")
+    pc.set_cmap("plasma")
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
 
-ax.set_xlim((-180, 180))
-ax.set_ylim((-90, 90))
-ax.add_collection(pc)
+    ax.set_xlim((-130, -60))
+    ax.set_ylim((20, 50))
 
-plt.title("Poly Collection Plot")
-plt.savefig('./MPAS_uxarray.png')
+    # add geographic features
+#    ax.add_feature(cfeature.COASTLINE)
+#    ax.add_feature(cfeature.BORDERS)
+
+    ax.add_collection(pc)
+
+    plt.title(f"{var} Plot")
+    plt.savefig(f'./MPAS_{var}.png')
+    plt.close()
