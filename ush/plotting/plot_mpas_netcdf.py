fac2141e647f4b65825ca60502ff4af918a85d57
Michael Kavulich, Jr <kavulich@ucar.edu>
2024-08-24 13:27:48 +0000

    More improvements:
     - No need to over-complicate logging, just have one stream that users
    can decide to put in a log file if they want
     - Move plotting loop under "elif" statement for variable list
     - Move latrange and lonrange back to plot: section
     - Add "title" as specifiable field in config file

--- ush/plotting/plot_mpas_netcdf.py
+++ ush/plotting/plot_mpas_netcdf.py
@@ -14,7 +14,7 @@ import cartopy.feature as cfeature
 
 import uwtools.api.config as uwconfig
 
-def load_dataset(config_d: dict,log: logging.Logger) -> ux.UxDataset:
+def load_dataset(config_d: dict) -> ux.UxDataset:
     """
     Program loads the dataset from the specified MPAS NetCDF file
     and returns it as a ux.UxDataset object
@@ -22,77 +22,79 @@ def load_dataset(config_d: dict,log: logging.Logger) -> ux.UxDataset:
 
     fn=config_d["data"]["filename"]
     grid = ux.open_grid(fn)
-    log.debug(grid)
+    logging.debug(grid)
 
     return ux.open_dataset(fn,fn)
 
-def plotit(config_d: dict,uxds: ux.UxDataset,log: logging.Logger) -> None:
+def plotit(config_d: dict,uxds: ux.UxDataset) -> None:
     """
     The main program that makes the plot(s)
     """
     fn=config_d["data"]["filename"]
     grid = ux.open_grid(fn)
-    log.debug(grid)
+    logging.debug(grid)
 
     uxds = ux.open_dataset(fn,fn)
 
-    # Each figure we will plot is a "polycollection"; we will first make a list of
-    # all the polycollections we want to plot, then plot each one in a later loop
-    pcs = []
 
     # Make sure the variable we want is present
     if config_d["data"]["var"]=="all":
         newconf = copy.deepcopy(config_d)
         for var in uxds:
             # To plot all variables, call plotit() recursively, trapping errors
-            log.info(f"Trying to plot variable {var}")
+            logging.info(f"Trying to plot variable {var}")
             newconf["data"]["var"]=[var]
             try:
-                plotit(newconf,uxds,log)
+                plotit(newconf,uxds)
             except Exception as e:
-                log.warning(e)
+                logging.warning(f"Could not plot variable {var}")
+                logging.warning(e)
         
     elif isinstance(config_d["data"]["var"], list):
         for var in config_d["data"]["var"]:
             if "n_face" not in uxds[var].dims:
-                log.info(f"Variable {var} not face-centered, skipping")
+                logging.info(f"Variable {var} not face-centered, skipping")
                 continue
-            log.info(f"Plotting variable {var}")
+            logging.info(f"Plotting variable {var}")
             sliced=uxds[var]
             if "Time" in sliced.dims:
-                log.info("Plotting first time step")
+                logging.info("Plotting first time step")
                 sliced=sliced.isel(Time=0)
             if "nVertLevels" in sliced.dims:
-                log.info("Plotting first vertical level")
+                logging.info("Plotting first vertical level")
                 sliced=sliced.isel(nVertLevels=0)
 
-            log.debug(sliced)
-            pcs.append(sliced.to_polycollection())
+            logging.debug(sliced)
+            pc=sliced.to_polycollection()
+
+            pc.set_antialiased(False)
+    
+            pc.set_cmap(config_d["plot"]["colormap"])
+    
+            fig, ax = plt.subplots(1, 1, figsize=(config_d["plot"]["figwidth"], config_d["plot"]["figheight"]),
+                                   dpi=config_d["plot"]["dpi"], constrained_layout=True)
+    
+    
+            ax.set_xlim((config_d["plot"]["lonrange"][0],config_d["plot"]["lonrange"][1]))
+            ax.set_ylim((config_d["plot"]["latrange"][0],config_d["plot"]["latrange"][1]))
+    
+            # add geographic features
+        #    ax.add_feature(cfeature.COASTLINE)
+        #    ax.add_feature(cfeature.BORDERS)
+    
+            ax.add_collection(pc)
+    
+            plottitle=config_d["plot"]["title"].format(var=var,lev=0)
+            plt.title(f"{var} Plot")
+#            plt.colorbar(plot,orientation='horizontal')
+            outfile=config_d["plot"]["filename"].format(var=var,lev=0)
+            plt.savefig(outfile)
+            plt.close()
+
+
     else:
         raise ValueError('Config value data:var must either be a list of variable names or the literal string "all"')
  
-    for pc in pcs:
-        pc.set_antialiased(False)
-    
-        pc.set_cmap(config_d["plot"]["colormap"])
-    
-        fig, ax = plt.subplots(1, 1, figsize=(config_d["plot"]["figwidth"], config_d["plot"]["figheight"]),
-                               dpi=config_d["plot"]["dpi"], constrained_layout=True)
-    
-
-        ax.set_xlim((config_d["data"]["lonrange"][0],config_d["data"]["lonrange"][1]))
-        ax.set_ylim((config_d["data"]["latrange"][0],config_d["data"]["latrange"][1]))
-    
-        # add geographic features
-    #    ax.add_feature(cfeature.COASTLINE)
-    #    ax.add_feature(cfeature.BORDERS)
-    
-        ax.add_collection(pc)
-    
-        plt.title(f"{var} Plot")
-        outfile=config_d["plot"]["filename"].format(var=var,lev=0)
-        plt.savefig(outfile)
-        plt.close()
 
 def setup_logging(logfile: str = "log.generate_FV3LAM_wflow", debug: bool = False) -> logging.Logger:
     """
@@ -130,10 +132,10 @@ def setup_logging(logfile: str = "log.generate_FV3LAM_wflow", debug: bool = Fals
     return logger
 
 
-def setup_config(config: str,log: logging.Logger) -> dict:
-    log.debug(f"Reading options file {config}")
+def setup_config(config: str) -> dict:
+    logging.debug(f"Reading options file {config}")
     config_d = uwconfig.get_yaml_config(config=config)
-    log.debug(f"Expanding references to other variables and Jinja templates")
+    logging.debug(f"Expanding references to other variables and Jinja templates")
     config_d.dereference()
     return config_d
 
@@ -151,12 +153,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    log=setup_logging(logfile="log.plot", debug=args.debug)
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
+
     # Load settings from config file
-    confg_d=setup_config(args.config,log)
+    confg_d=setup_config(args.config)
 
     # Open specified file and load dataset
-    dataset=load_dataset(confg_d,log)
+    dataset=load_dataset(confg_d)
 
     # Make the plots!
-    plotit(confg_d,dataset,log)
+    plotit(confg_d,dataset)
